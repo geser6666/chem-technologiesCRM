337516b52478c216bfd7790a3b6663bdee0df505
Serge <notiam@list.ru>
2013-11-19 11:18:05 +0200

    Beginning

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChemTechnologiescrm::Application.config.secret_token = '49515b9f5b326ca7b7fec617e4e8a1310ca7e60345867239af577f8e1a439489f142cb9e476087370f6ca0b294405061fbd68127c9073478bc32a8f4a06b14ea'
