67271284c4d641d996b35490f255b865c8a2dbaf
Sergey P <ser_pop@list.ru>
2013-12-05 21:36:23 +0200

    Adminpanel2 country tests

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,4 +1,6 @@
 class Country < ActiveRecord::Base
   attr_accessible :code, :name
   has_many :clients
+  validates  :code, presence: true
+  validates  :name, presence: true
 end
--- spec/models/country_spec.rb
+++ spec/models/country_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe Country do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before { @country = Country.new(name: "Test Country", code: "TC") }
+
+  subject { @country }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:code) }
+
+   it { should be_valid }
+
+  describe "when name is not present" do
+    before { @country.name = " " }
+    it { should_not be_valid }
+  end
+  describe "when code is not present" do
+    before { @country.code = " " }
+    it { should_not be_valid }
+  end
+
+
+
 end
