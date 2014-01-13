afe038cf77fe79599707bb2657bcf3a958dba86c
Serge <notiam@list.ru>
2014-01-13 16:36:25 +0200

    ne ochen rabochee

--- app/controllers/negotiations_controller.rb
+++ app/controllers/negotiations_controller.rb
@@ -1,8 +1,13 @@
 class NegotiationsController < ApplicationController
+	include NegotiationsHelper
   def new
   end
   def index
     @clients = Client.paginate(page: params[:page])	
+	if !params[:format].nil?
+		@client = Client.find(params[:format])
+	    @negotiations = @client.negotiations
+	end
   end
   
 end
--- spec/requests/negotiations_pages_spec.rb
+++ spec/requests/negotiations_pages_spec.rb
@@ -10,48 +10,59 @@ describe "NegotiationsPages" do
     it { should have_content('Вход') }
   end
   describe "index" do
-		before do
-			sign_in FactoryGirl.create(:user)
-			FactoryGirl.create(:client, name: "Bob", address: "Луганск")
-			FactoryGirl.create(:client, name: "Ben", address: "Луганск")
-			visit negotiations_path
-		end
+    before do
+            sign_in user
+            FactoryGirl.create(:client, name: "Bob", address: "Луганск")
+            FactoryGirl.create(:client, name: "Ben", address: "Луганск")
+            visit negotiations_path
+    end
 
-		it { should have_selector('title',	text: "Переговоры") }
-		it { should have_selector('h4',		text: "Список клиентов") }
+    it { should have_selector('title',        text: "Переговоры") }
+    it { should have_selector('h4',                text: "Список клиентов") }
 
-		it "Should list each client" do
-			Client.all.each do |client|
-				page.should have_selector('li', text: client.name)
-			end
-		end
+    it "Should list each client" do
+            Client.all.each do |client|
+                    page.should have_selector('li', text: client.name)
+            end
+    end
 
-		describe "pagination" do
-			before(:all) { 35.times { FactoryGirl.create(:client) } }
-			after(:all)  { Client.delete_all }
+    describe "pagination" do
+            before(:all) { 35.times { FactoryGirl.create(:client) } }
+            after(:all) { Client.delete_all }
 
-			it { should have_selector('div.pagination') }
+            it { should have_selector('div.pagination') }
 
-			it "should list each user" do
-				Client.paginate(page: 1).each do |client|
-					page.should have_selector('li', text: client.name)
-				end
-			end
-		end
+            it "should list each user" do
+                    Client.paginate(page: 1).each do |client|
+                            page.should have_selector('li', text: client.name)
+                    end
+            end
+    end
+	# describe "right links of clients" do
+	# 	before(:all) { 35.times { FactoryGirl.create(:client) } }
+	# 	after(:all)  { Client.delete_all }
 
-		describe "right links of clients" do
-			
-		end
+	# 	Client.all.each do |client|
+	# 		#page.should have_link(client.name, href: negotiations_path(client)) 
+	# 	end
+		
+	# end
+	# describe "get right negotiations " do
+	# 	#let(:client) { FactoryGirl.create(:client, name: "Bob", address: "Луганск") }
+	# 	# client.negotiations.build(value:'Negotiation 1')
+	# 	# visit negotiations_path(client)
+	# 	# page.should have_selector('li', text: "Negotiation 1")
+	# end
 
 
-		# describe "toggling the button" do
-  #         before { click_button "Получать" }
-  #         it { should have_selector('input', value: 'Не получать') }
-	 #    end
-		# describe "кнопка редактирования" do
-  #     		it { should have_link('', href: edit_client_path(Client.first)) }
-  #   	end
-	end
+    # describe "toggling the button" do
+# before { click_button "Получать" }
+# it { should have_selector('input', value: 'Не получать') }
+# end
+    # describe "кнопка редактирования" do
+#                 it { should have_link('', href: edit_client_path(Client.first)) }
+#         end
+        end
 
 
 
